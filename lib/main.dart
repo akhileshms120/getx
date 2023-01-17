5f6ce77c1b38c23479bf3dfede047039ca4afcde
akhileshms120 <akhileshcommand@gmail.com>
2023-01-17 21:05:43 +0530

    single obs and full class obs

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:second_project/student.dart';
 
 //////////////////////////////////state Manger///////////////////
 ///
@@ -19,15 +20,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp();
+    return GetMaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
 class HomeScreen extends StatelessWidget {
+
+  var student=Student();
  
  //var countNumber=RxString(''); //one method
 //  var countNumber=Rx<String>(''); //second method
 
- var number=RxNum(0);
+// var number=RxNum(0);
+
+ var count =0.obs;
+
+ var name ="Tom".obs;
+
+ increment(){
+  count++;
+}
+
+ decrement(){
+  count--;
+}
 
   @override
   Widget build(BuildContext context) {
@@ -35,11 +52,30 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(title: Text("State Mangament")),
       body: Column(
         children: [
-          Obx(()=>Text("Count Value is 0")),
-          ElevatedButton(onPressed: (){}, child: Text("Increment")),
-             ElevatedButton(onPressed: (){}, child: Text("Decrement"))
+          Obx(()=>Text("Count Value is $count  $name")),
+          ElevatedButton(onPressed: (){
+            increment();
+          }, child: Text("Increment")),
+             ElevatedButton(onPressed: (){
+              decrement();
+             }, child: Text("Decrement")),
+               ElevatedButton(onPressed: (){
+               
+               Get.to(()=>StudentScreen());
+             }, child: Text("Student Page"))
         ],
       ),
     );
   }
+}
+
+class Student{
+
+  // var name= "Tom".obs; //individual value is reactive 
+  // var age =25.obs;
+
+  var name;
+  var age;
+
+  Student({ this.name, this.age});
 }
